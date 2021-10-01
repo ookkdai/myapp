373e69e67b9ff24163a34607822f6298f437d3dc
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-10-01 10:41:09 +0900

    postsコントローラーを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
   end
+  def new
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
 end
