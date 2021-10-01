01e066a99c4980ad64c57bbcc180a5398641929c
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-10-01 14:20:03 +0900

    詳細、更新、削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(id: :asc)
   end
   def new
     @post = Post.new
@@ -8,14 +9,21 @@ class PostsController < ApplicationController
     post = Post.create!(post_params)
     redirect_to post
   end
+  def show
+    @post = Post.find(params[:id])
+  end
   def edit
-    
+    @post = Post.find(params[:id])
   end
   def update
-    
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
   def destroy
-    
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
   end
   private
 
